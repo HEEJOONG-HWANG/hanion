c688fbf0c3fa8f1087f59cef7c49718bb854b64b
ssy0622 <39024205+ssy0622@users.noreply.github.com>
2018-06-18 19:08:08 +0900

    influxdb랑 python 연동

--- python crolling/crolltest.py
+++ python crolling/crolltest.py
@@ -69,13 +69,105 @@ print(bsObj) #test ㅜㅜ
 ########### api 세계날씨 https://openweathermap.org/api #################################################################################################
 
 # 내 api key    -     11335f1806a57d3a529433bf55ed39e8
-
+"""
 from urllib.request import urlopen # urllib라이브러리에서 파이썬 모듈 requst를 읽어 open함수 하나만 임포트
 from bs4 import BeautifulSoup
 html = urlopen("http://api.openweathermap.org/data/2.5/forecast?id=1835847&APPID=11335f1806a57d3a529433bf55ed39e8&mode=xml") # &mode=xml  seoul
 #api 세계날씨
 bsObj = BeautifulSoup(html.read(),"lxml")
 print(bsObj) #test 
-
+"""
 
 ########  세계날씨#############################################################
+
+
+
+
+####################influxdb 연동 ################################################
+
+############ 라이브러리를 설치해 줘야한다
+"""
+$ pip install influxdb
+$ pip install --upgrade influxdb
+$ pip uninstall influxdb
+
+"""
+############# tutorial - basic ##########################################################
+
+# -*- coding: utf-8 -*-
+"""Tutorial on using the InfluxDB client."""
+
+import argparse
+
+from influxdb import InfluxDBClient
+
+
+def main(host='localhost', port=8086):
+    """Instantiate a connection to the InfluxDB."""
+    user = 'root'
+    password = 'root'
+    dbname = 'example'
+    dbuser = 'smly'
+    dbuser_password = 'my_secret_password'
+    query = 'select value from cpu_load_short;'
+    json_body = [
+        {
+            "measurement": "cpu_load_short",
+            "tags": {
+                "host": "server01",
+                "region": "us-west"
+            },
+            "time": "2009-11-10T23:00:00Z",
+            "fields": {
+                "Float_value": 0.64,
+                "Int_value": 3,
+                "String_value": "Text",
+                "Bool_value": True
+            }
+        }
+    ]
+
+    client = InfluxDBClient(host, port, user, password, dbname)
+
+    print("Create database: " + dbname)
+    client.create_database(dbname)
+
+    print("Create a retention policy")
+    client.create_retention_policy('awesome_policy', '3d', 3, default=True)
+
+    print("Switch user: " + dbuser)
+    client.switch_user(dbuser, dbuser_password)
+
+    print("Write points: {0}".format(json_body))
+    client.write_points(json_body)
+
+    print("Querying data: " + query)
+    result = client.query(query)
+
+    print("Result: {0}".format(result))
+
+    print("Switch user: " + user)
+    client.switch_user(user, password)
+
+    print("Drop database: " + dbname)
+    client.drop_database(dbname)
+
+
+def parse_args():
+    """Parse the args."""
+    parser = argparse.ArgumentParser(
+        description='example code to play with InfluxDB')
+    parser.add_argument('--host', type=str, required=False,
+                        default='localhost',
+                        help='hostname of InfluxDB http API')
+    parser.add_argument('--port', type=int, required=False, default=8086,
+                        help='port of InfluxDB http API')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(host=args.host, port=args.port)
+
+
+######### tutorial - basic #############################
